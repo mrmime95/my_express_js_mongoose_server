46c4022a5aa4fdfe98bb1a88afc31ab79731f9d5
Gyorgy Szilard <gyorgys@gmail.com>
2020-02-01 15:31:40 +0200

    User registration started, fields validations added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,27 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const { check, validationResult } = require('express-validator/check')
 
-// @route     GET api/users
-// @desc      Test route
+// @route     POST api/users
+// @desc      Register route
 // @access    Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+	'/',
+	[
+		check('name', 'Name is requred')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+	],
+	(req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+		console.log(req.body)
+		res.send('User route')
+	},
+)
 
-module.exports = router;
+module.exports = router
