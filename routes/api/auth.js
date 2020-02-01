711a4fcecc364d713bd2491119f5e3b36a7e0a58
Gyorgy Szilard <gyorgys@gmail.com>
2020-02-01 16:42:45 +0200

    Login added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
 
 // @route     GET api/auth
-// @desc      Test route
+// @desc      Get Authenticated user
 // @access    Protected
 router.get('/', auth, async (req, res) => {
 	try {
@@ -16,5 +20,58 @@ router.get('/', auth, async (req, res) => {
 		res.status(500).send('Server Error')
 	}
 })
+
+// @route     POST api/auth
+// @desc      Authenticate user and get token
+// @access    Public
+router.post(
+	'/',
+	[check('email', 'Please include a valid email').isEmail(), check('password', 'Password is required').exists()],
+	async (req, res) => {
+		// data validation
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const { email, password } = req.body
+		try {
+			// see if the user exists
+			let user = await User.findOne({ email })
+			if (!user) {
+				return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] })
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password)
+			if (!isMatch) {
+				return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] })
+			}
+
+			// return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			}
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000, //TODO:change to 3600
+				},
+				(err, token) => {
+					if (err) {
+						throw err
+					}
+					res.json({ token })
+				},
+			)
+		} catch (err) {
+			console.error(err.message)
+			return res.status(500).send('Server error')
+		}
+	},
+)
 
 module.exports = router
