727e50305788e6750b5109fbce75a941eced8a2c
Gyorgy Szilard <gyorgys@gmail.com>
2020-02-01 16:08:29 +0200

    JWT added and sent back as response for the requests

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 const User = require('../../models/User')
@@ -47,8 +49,25 @@ router.post(
 			await user.save()
 
 			// return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			}
 
-			res.send('User registered')
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000, //TODO:change to 3600
+				},
+				(err, token) => {
+					if (err) {
+						throw err
+					}
+					res.json({ token })
+				},
+			)
 		} catch (err) {
 			console.error(err)
 			return res.status(500).send('Server error')
