1ace4543bba8660ea8721fb191adaeb763be2e7d
Gyorgy Szilard <gyorgys@gmail.com>
2020-02-01 14:55:20 +0200

    user schema created, also some prettier thing added

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express')
+const connectDB = require('./config/db')
 
-const app = express();
+const app = express()
 
 //Connect database
-connectDB();
+connectDB()
 
-app.get("/", (req, res) => res.send("API running"));
+app.get('/', (req, res) => res.send('API running'))
 
 // Define Routes
 
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/profile", require("./routes/api/profile"));
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
-});
+	console.log(`Server started on ${PORT}`)
+})
